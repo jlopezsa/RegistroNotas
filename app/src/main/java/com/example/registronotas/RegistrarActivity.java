e25b30a1537fecc5587e195eba100d42f038539b
Julian Lopez <jlopezsa@gmail.com>
2021-09-30 22:09:55 -0500

    Actividad Registrar adicionada

---
+++ app/src/main/java/com/example/registronotas/RegistrarActivity.java
@@ -0,0 +1,13 @@
+package com.example.registronotas;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RegistrarActivity extends Activity {
+
+    @Override public void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_registrar);
+    }
+
+}
